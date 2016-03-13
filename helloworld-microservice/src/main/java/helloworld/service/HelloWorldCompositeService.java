30b9d46e5d9a559b35124b3e36ef65b2a18e19bd
Leopold <dauvergne.leopold@gmail.com>
2016-03-13 09:42:23 +0100

    feat:personal helloworld

--- helloworld-microservice/src/main/java/helloworld/service/HelloWorldCompositeService.java
+++ helloworld-microservice/src/main/java/helloworld/service/HelloWorldCompositeService.java
@@ -3,10 +3,13 @@ package helloworld.service;
 
 import java.util.Date;
 
+import javax.ws.rs.GET;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,11 @@ public class HelloWorldCompositeService {
         return "{\"timestamp\":\"" + new Date() + "\",\"content\":\"Hello from Hello Service\"}";
     }
 
-    @RequestMapping("/hello")
-    public ResponseEntity<String> mSayHello() {
+    @GET
+    @RequestMapping("/hello/{user}")
+    public ResponseEntity<String> mSayHello(@PathVariable String user) {
 
        
-        return util.createOkResponse(new String("Helloworld"));
+        return util.createOkResponse(new String("Hello "+user));
     }
 }
